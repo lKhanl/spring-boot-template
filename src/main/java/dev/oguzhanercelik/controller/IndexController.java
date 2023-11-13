71aa4a1d4e8019ee3fac787c9b6addb860cfaac8
Khan <oguzhanercelik@eskisehir.edu.tr>
2023-11-13 21:26:10 +0300

    updated

--- src/main/java/dev/oguzhanercelik/controller/IndexController.java
+++ src/main/java/dev/oguzhanercelik/controller/IndexController.java
@@ -1,11 +1,12 @@
 package dev.oguzhanercelik.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.view.RedirectView;
 
-@RestController
+@Controller
 @RequestMapping("/")
 public class IndexController {
 
